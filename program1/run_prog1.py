2153827475b1495390de7b61730d5a051cbe42ad
Josh Kluthe <kluthej@oregonstate.edu>
2017-04-15 18:51:47 -0700

    add program1

---
+++ program1/run_prog1.py
@@ -0,0 +1,21 @@
+#/usr/bin/env python3
+
+import os
+
+
+def main():
+	datafile = "data.csv"
+	#create or truncate the file
+	open(datafile, "w").close()
+	for threads in [1, 2, 4, 8]:
+		for nodes in [10, 100, 1000, 10000]:
+			cmd = ("g++ -DNUMTHREADS={} -DNUMNODES={} program1.cpp -o "
+			"program1 -lm -fopenmp").format(threads, nodes)
+			os.system(cmd)
+			cmd = "./program1 {}".format(datafile)
+			os.system(cmd)
+
+
+if __name__ == "__main__":
+	main()
+
